b729a90eb782f4e757063d46c485c5517c0a0642
andreac01 <andrea01.cerutti@gmail.com>
2024-12-06 23:55:25 +0100

    updated scraper

--- scraper.sh
+++ scraper.sh
@@ -5,6 +5,11 @@ mkdir -p data
 
 # URL of the .ics file
 url="URL TO DOWNLOAD YOUR CALENDAR.ics FILE"
+if [ "$url" == "URL TO DOWNLOAD YOUR CALENDAR.ics FILE" ]; then
+	echo "The URL has not been set. Please provide the URL to download your calendar .ics file:"
+	read url
+	echo "remember to set the URL in the script!s"
+fi
 
 # Destination path
 destination="data/basic.ics"
